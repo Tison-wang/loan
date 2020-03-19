2f01492f6f8445d5d46bbbe4e7b1d61e7db6a94f
tiansheng.wang <Wts#519048559>
2020-03-19 11:08:44 +0800

    add lintOnSave : false

--- vue-loan-platform/vue.config.js
+++ vue-loan-platform/vue.config.js
@@ -1,93 +1,94 @@
 module.exports = {
-  // publicPath: process.env.NODE_ENV === "production" ? "../../" : "/",
-  // publicPath: "/",
-  devServer: {
-    host: 'localhost',
-    port: '81',
-    hot: true,
-    open: true,
-    overlay: {
-      warning: false,
-      error: true
+    // publicPath: process.env.NODE_ENV === "production" ? "../../" : "/",
+    // publicPath: "/",
+    lintOnSave: false,
+    devServer: {
+        host: 'localhost',
+        port: '81',
+        hot: true,
+        open: true,
+        overlay: {
+            warning: false,
+            error: true
+        },
+        disableHostCheck: true
+        // proxy: {
+        //   [process.env.VUE_APP_BASE_API]: {
+        //     target: process.env.VUE_APP_BASE_API,
+        //     changeOrigin: true,
+        //     secure: false,
+        //     pathRewrite: {
+        //       ["^" + process.env.VUE_APP_BASE_API]: ""
+        //     }
+        //   }
+        // }
     },
-    disableHostCheck: true
-    // proxy: {
-    //   [process.env.VUE_APP_BASE_API]: {
-    //     target: process.env.VUE_APP_BASE_API,
-    //     changeOrigin: true,
-    //     secure: false,
-    //     pathRewrite: {
-    //       ["^" + process.env.VUE_APP_BASE_API]: ""
-    //     }
-    //   }
-    // }
-  },
-  css: {
-    // 是否使用css分离插件 ExtractTextPlugin
-    extract: true,
-    // 开启 CSS source maps?
-    sourceMap: false,
-    // css预设器配置项
-    loaderOptions: {},
-    // 启用 CSS modules for all css / pre-processor files.
-    modules: false
-  },
-  configureWebpack: webackConfig => {
-    const config = webackConfig
-    const optimization = {}
+    css: {
+        // 是否使用css分离插件 ExtractTextPlugin
+        extract: true,
+        // 开启 CSS source maps?
+        sourceMap: false,
+        // css预设器配置项
+        loaderOptions: {},
+        // 启用 CSS modules for all css / pre-processor files.
+        modules: false
+    },
+    configureWebpack: webackConfig => {
+        const config = webackConfig
+        const optimization = {}
 
-    if (process.env.NODE_ENV === 'production') {
-      // dop console
-      config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
+        if (process.env.NODE_ENV === 'production') {
+            // dop console
+            config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
 
-      // 抽離公共代碼
-      optimization.splitChunks = {
-        cacheGroups: {
-          vendor: {
-            chunks: 'all',
-            test: /node_modules/,
-            name: 'vendor',
-            minChunks: 1,
-            maxInitialRequests: 5,
-            minSize: 0,
-            priority: 100
-          },
-          common: {
-            chunks: 'all',
-            test: /[\\/]src[\\/]js[\\/]/,
-            name: 'common',
-            minChunks: 2,
-            maxInitialRequests: 5,
-            minSize: 0,
-            priority: 60
-          },
-          styles: {
-            name: 'styles',
-            test: /\.(sa|sc|c)ss$/,
-            chunks: 'all',
-            enforce: true
-          },
-          runtimeChunk: {
-            name: 'manifest'
-          }
+            // 抽離公共代碼
+            optimization.splitChunks = {
+                cacheGroups: {
+                    vendor: {
+                        chunks: 'all',
+                        test: /node_modules/,
+                        name: 'vendor',
+                        minChunks: 1,
+                        maxInitialRequests: 5,
+                        minSize: 0,
+                        priority: 100
+                    },
+                    common: {
+                        chunks: 'all',
+                        test: /[\\/]src[\\/]js[\\/]/,
+                        name: 'common',
+                        minChunks: 2,
+                        maxInitialRequests: 5,
+                        minSize: 0,
+                        priority: 60
+                    },
+                    styles: {
+                        name: 'styles',
+                        test: /\.(sa|sc|c)ss$/,
+                        chunks: 'all',
+                        enforce: true
+                    },
+                    runtimeChunk: {
+                        name: 'manifest'
+                    }
+                }
+            }
         }
-      }
-    }
-    return {
-      optimization
-    }
-  },
-  // 压缩图片
-  // chainWebpack: config => {
-  //   config.module
-  //     .rule('images')
-  //     .test(/\.(png|jpe?g|gif|svg)(\?.*)?$/)
-  //     .use('image-webpack-loader')
-  //     .loader('image-webpack-loader')
-  //     .options({
-  //       bypassOnDebug: true
-  //     })
-  //     .end()
-  // },
-  productionSourceMap: false
+        return {
+            optimization
+        }
+    },
+    // 压缩图片
+    // chainWebpack: config => {
+    //   config.module
+    //     .rule('images')
+    //     .test(/\.(png|jpe?g|gif|svg)(\?.*)?$/)
+    //     .use('image-webpack-loader')
+    //     .loader('image-webpack-loader')
+    //     .options({
+    //       bypassOnDebug: true
+    //     })
+    //     .end()
+    // },
+    productionSourceMap: false
 }
